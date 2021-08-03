a802ef56916109fa332302ed96ffdffc70e6d895
davide vanoncini <davidevanoncini2003@gmail.com>
2021-08-03 10:45:26 +0200

    modifying go mod to deploy on heroku, working on homepage

--- go.mod
+++ go.mod
@@ -1,5 +1,6 @@
 module github.com/Vano2903/peggle-go-thingy-data
 
+// +heroku goVersion go1.11
 go 1.16
 
 require (
--- static/js/card.js
+++ static/js/card.js
@@ -1,8 +1,10 @@
+var last_date = undefined
+
 async function fetchCardData(date) {
-    url = '/games/search?title=&limit=20'
+    url = '/games/search?limit=20'
     console.log(date)
-    if (date === undefined) {
-        
+    if (date !== undefined) {
+        url = `/games/search?upload=${date}&limit=20`
     }
     const res = await fetch(url, {
         method: "GET"
