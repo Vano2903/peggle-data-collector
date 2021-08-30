1d2815d1f026daf14133c7f036b593c87b5cf701
davide vanoncini <davidevanoncini2003@gmail.com>
2021-08-31 00:45:12 +0200

    problem with extrem fever and changed behavoir of js in loadGame (pfp) changed name of images cause windows ;-;

--- static/js/game/loadGame.js
+++ static/js/game/loadGame.js
@@ -1,6 +1,6 @@
 "use strict"
 document.getElementById("loader-wrapper").style.display = "block";
-window.onload = async function(){
+window.onload = async function () {
     await genPage()
     document.getElementById("loader-wrapper").style.display = "none";
 }
@@ -9,13 +9,13 @@ let id = window.location.pathname.slice(1);
 let gameData;
 document.getElementsByTagName('title')[0].innerHTML += id;
 
-async function fetchGameData(){
+async function fetchGameData() {
     let url = `/games/search?id=${id}`;
     const res = await fetch(url, {
         method: "GET"
     });
     const status = res.status;
-    if (status === 404){
+    if (status === 404) {
         // window.location = "/404"
         const res = await fetch('/404', {
             method: "GET"
@@ -29,31 +29,32 @@ async function fetchGameData(){
     return resp[0];
 }
 
-async function getUserProfilePicture(name){
-    let url = "/users/pfp/"+ name;
+async function getUserProfilePicture(name) {
+    let url = "/users/pfp/" + name;
     const res = await fetch(url, {
         method: "GET"
     });
     const status = res.status;
-    if (status === 400){
+    if (status === 400) {
         return ""
     }
     const resp = await res.json();
-    return resp.url;
+    console.log(resp[0])
+    return resp[0];
 }
 
-async function genPage(){
+async function genPage() {
     gameData = await fetchGameData();
     document.getElementById("title").innerHTML = gameData.videoData.title;
     if (gameData.wonBy == 1) {
         document.getElementById("scrown").style.display = "block";
-    }else if (gameData.wonBy == 0) {
+    } else if (gameData.wonBy == 0) {
         document.getElementById("rcrown").style.display = "block";
-    }else{
+    } else {
         document.getElementById("scrown").style.display = "block";
         document.getElementById("rcrown").style.display = "block";
     }
-    for (let i = 0; i < 4; i++){
+    for (let i = 0; i < 4; i++) {
         setSection(i, gameData)
     }
     document.getElementById("youtube-embed").src = `https://www.youtube.com/embed/${id}?rel=0`
@@ -61,8 +62,8 @@ async function genPage(){
     document.getElementById("userPfp").src = await getUserProfilePicture(gameData.addedBy);
 }
 
-function hideAllSections(){
-    for(let i = 0; i < 4; i++){
+function hideAllSections() {
+    for (let i = 0; i < 4; i++) {
         document.getElementById(`par${i}`).style.display = "none";
         document.getElementById(`labButPar${i}`).classList.remove("btn-outline-secondary");
         document.getElementById(`labButPar${i}`).classList.remove("btn-outline-success");
@@ -70,31 +71,31 @@ function hideAllSections(){
     }
 }
 
-function showSection(section){
+function showSection(section) {
     hideAllSections()
     document.getElementById(`par${section}`).style.display = "block";
     document.getElementById(`labButPar${section}`).classList.remove("btn-outline-secondary");
     document.getElementById(`labButPar${section}`).classList.add("btn-outline-success");
 }
 
-function setSection(index, game){
-    if (index === 0){
+function setSection(index, game) {
+    if (index === 0) {
         document.getElementById("stpoints").innerHTML += game.stats.synergo.overall.tPoints;
         document.getElementById("st25").innerHTML += game.stats.synergo.overall.t25;
         document.getElementById("rtpoints").innerHTML += game.stats.redez.overall.tPoints;
         document.getElementById("rt25").innerHTML += game.stats.redez.overall.t25;
-    }else{
-        document.getElementById(`s${index}points`).innerHTML += game.stats.synergo["g"+index].points;
-        document.getElementById(`s${index}25`).innerHTML += game.stats.synergo["g"+index].n25;
-        document.getElementById(`s${index}fe`).innerHTML += game.stats.synergo["g"+index].valFe;
-        document.getElementById(`s${index}charName`).innerHTML += game.stats.synergo["g"+index].character;
-        document.getElementById(`s${index}charImg`).src = "/static/images/" + game.stats.synergo["g"+index].character.slice(0, 3)+".png";
+    } else {
+        document.getElementById(`s${index}points`).innerHTML += game.stats.synergo["g" + index].points;
+        document.getElementById(`s${index}25`).innerHTML += game.stats.synergo["g" + index].n25;
+        document.getElementById(`s${index}fe`).innerHTML += game.stats.synergo["g" + index].valFe;
+        document.getElementById(`s${index}charName`).innerHTML += game.stats.synergo["g" + index].character;
+        document.getElementById(`s${index}charImg`).src = "/static/images/" + game.stats.synergo["g" + index].character.slice(0, 4) + ".png";
 
-        document.getElementById(`r${index}points`).innerHTML += game.stats.redez["g"+index].points;
-        document.getElementById(`r${index}25`).innerHTML += game.stats.redez["g"+index].n25;
-        document.getElementById(`r${index}fe`).innerHTML += game.stats.redez["g"+index].valFe;
-        document.getElementById(`r${index}charName`).innerHTML += game.stats.redez["g"+index].character;
-        document.getElementById(`r${index}charImg`).src = "/static/images/" + game.stats.redez["g"+index].character.slice(0, 3)+".png";
+        document.getElementById(`r${index}points`).innerHTML += game.stats.redez["g" + index].points;
+        document.getElementById(`r${index}25`).innerHTML += game.stats.redez["g" + index].n25;
+        document.getElementById(`r${index}fe`).innerHTML += game.stats.redez["g" + index].valFe;
+        document.getElementById(`r${index}charName`).innerHTML += game.stats.redez["g" + index].character;
+        document.getElementById(`r${index}charImg`).src = "/static/images/" + game.stats.redez["g" + index].character.slice(0, 4) + ".png";
     }
 }
 
